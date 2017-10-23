12809e5c5f5ef3a8a41b29a2c82f535f3b266f30
fosb <fosbtank@gmail.com>
2017-10-23 18:44:05 +0100

    spell names scraped

--- scraping/spells.R
+++ scraping/spells.R
@@ -4,8 +4,4 @@ library(dplyr)
 
 spells_raw <- read_html("http://engl393-dnd5th.wikia.com/wiki/Spells")
 
-spells_names_raw <- html_nodes(spells_raw, ".mw-content-text h3") %>% html_text()
-
-spell_names_list <- html_nodes(spells_names_raw, "ul") %>% html_text()
-
-spell_names <- html_nodes(spell_names_list, "li") %>% html_text()
+spells_names_raw <- html_nodes(spells_raw, ".mw-content-text ul li") %>% html_text()
