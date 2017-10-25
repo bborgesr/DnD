3827c7dfde4239e64f4556236cc3182127baaf1f
fosb <fosbtank@gmail.com>
2017-10-25 16:15:56 +0100

    spells scrape
    spell names strings corrected
    problem with reading the url for each particular spell

--- scraping/spells.R
+++ scraping/spells.R
@@ -5,3 +5,11 @@ library(dplyr)
 spells_raw <- read_html("http://engl393-dnd5th.wikia.com/wiki/Spells")
 
 spells_names_raw <- html_nodes(spells_raw, ".mw-content-text ul li") %>% html_text()
+
+spells_names <- sub("\\[.*", "", spells_names_raw)
+spells_names <- sub("\\(.*", "", spells_names)
+
+for(i in 1:length(spells_names)){
+  url <- paste("http://engl393-dnd5th.wikia.com/wiki/", spells_names[i], sep = "")
+  spells_pages_raw <- read_html(url)
+}
