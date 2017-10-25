ffe114699ea4b3374043c856c1215608dd86916f
sraifos <sofiarodriguesilva@gmail.com>
2017-10-25 16:46:48 +0100

    Backgrounds scraping
    backgrounds_names has been trimmed from white spaces except right space

---
+++ scraping/backgrounds.R
@@ -0,0 +1,10 @@
+library(rvest)
+library(tibble)
+library(dplyr)
+
+backgrounds_raw <- read_html("http://engl393-dnd5th.wikia.com/wiki/Backgrounds")
+
+backgrounds_names_raw <- html_nodes(backgrounds_raw, ".mw-content-text ul li") %>% html_text()
+
+backgrounds_names <- sub("\\(.*", "", backgrounds_names_raw)
+trimws(backgrounds_names, "l")
