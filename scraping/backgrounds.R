335681564546b03f6583cb074aa084c05a7703e9
sraifos <sofiarodriguesilva@gmail.com>
2017-10-30 16:29:11 +0000

    -

--- scraping/backgrounds.R
+++ scraping/backgrounds.R
@@ -4,16 +4,9 @@ library(dplyr)
 
 backgrounds_raw <- read_html("http://engl393-dnd5th.wikia.com/wiki/Backgrounds")
 
-<<<<<<< HEAD
 backgrounds_names_raw <- html_nodes(backgrounds_raw, ".mw-content-text ul~ul~ul~ul~ul li") %>% html_text()
 
 backgrounds_names <- sub("\\(.*", "", backgrounds_names_raw)
 trimws(backgrounds_names, which = c("both"))
        
 backgrounds_names_url <- sub("\\ ", "", backgrounds_names)
-=======
-backgrounds_names_raw <- html_nodes(backgrounds_raw, ".mw-content-text ul li") %>% html_text()
-
-backgrounds_names <- sub("\\(.*", "", backgrounds_names_raw)
-trimws(backgrounds_names, "l")
->>>>>>> ffe114699ea4b3374043c856c1215608dd86916f
