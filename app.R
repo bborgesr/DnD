8df8be00672cef44db1b33d281bcedebd262f18f
fosb <fosbtank@gmail.com>
2017-12-01 15:55:21 +0000

    mockup PC dashboard

--- app.R
+++ app.R
@@ -1,1 +1,19 @@
-#Hai
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "PC"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Character Sheet"),
+      menuItem("Notes"),
+      menuItem("Dice Roller"),
+      menuItem("Database")
+    )
+  ),
+  dashboardBody()
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
