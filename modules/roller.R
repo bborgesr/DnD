56ca2d829fc707627c8f44c041bac80db6d74278
fosb <fosbtank@gmail.com>
2018-01-18 16:25:01 +0000

    last try at roller by me

--- modules/roller.R
+++ modules/roller.R
@@ -2,10 +2,12 @@
 rollerModUI <- function(id) {
   ns <- NS(id)
   tagList(
-    
+    actionButton(ns("d4"), "Randomize a d4")
   )
 }
 
 rollerMod <- function(input, output, session) {
-  
+  observeEvent(input$d4, {
+    output$rd4 <- sample(1:4)  
+  })
 }
