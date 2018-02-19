7f31ad9b62663b81e222058144a933447ac41c17
fosb <fosbtank@gmail.com>
2018-02-19 18:45:04 +0000

    roller updates

--- modules/roller.R
+++ modules/roller.R
@@ -2,12 +2,13 @@
 rollerModUI <- function(id) {
   ns <- NS(id)
   tagList(
-    actionButton(ns("d4"), "Randomize a d4")
+    actionButton(ns("d4"), "Randomize a d4"),
+    textOutput("rd4")
   )
 }
 
 rollerMod <- function(input, output, session) {
   observeEvent(input$d4, {
-    output$rd4 <- sample(1:4)  
+    output$rd4 <- renderText(sample(1:4, 1, replace = TRUE))
   })
 }
